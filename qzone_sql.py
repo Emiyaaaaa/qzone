4d58eb5d025410e1b3fcefd79e66b8e2b2acb62a
Emiyaaaaa <2914034404@qq.com>
2018-12-30 16:15:55 +0800

    bug fixed

--- qzone_sql.py
+++ qzone_sql.py
@@ -40,8 +40,8 @@ class GetQzoneToMysql(object):
         cursor.execute('SELECT id FROM qzone_html')
         id_all = cursor.fetchall()
         if id_all == ():
-            sql = 'INSERT INTO qzone_html (html,time,upload_time) VALUES (%s,%s,%s)'
-            cursor.execute(sql, ('lhz', '1970年1月1日 08:00','1970年1月1日 08:00'))
+            sql = 'INSERT INTO qzone_html (html,time,upload_time,is_turned) VALUES (%s,%s,%s,%s)'
+            cursor.execute(sql, ('lhz', '1970年1月1日 08:00','1970年1月1日 08:00',0))
             connect.commit()
         return cursor,connect
 
@@ -60,10 +60,11 @@ class GetQzoneToMysql(object):
 
         timeArray = time.strptime(time_, "%Y年%m月%d日 %H:%M")
         time__ = time.mktime(timeArray)
-        new_time = self.new_time(time__, sql_new_time[0])
-        if new_time == time__ and new_time != sql_new_time[0]:
-            sql = 'INSERT INTO qzone_html (html,time,upload_time) VALUES (%s,%s,%s)'
-            cursor.execute(sql, (text, time_,upload_time))
+        sql_new_time_ = time.mktime(time.strptime(sql_new_time[0][0], "%Y年%m月%d日 %H:%M"))
+        new_time = self.new_time(time__, sql_new_time[0][0])
+        if new_time == time__ and new_time != sql_new_time_:
+            sql = 'INSERT INTO qzone_html (html,time,upload_time,is_turned) VALUES (%s,%s,%s,%s)'
+            cursor.execute(sql, (text, time_,upload_time,0))
             connect.commit()
             return 'ok'
         else:
@@ -114,7 +115,7 @@ class GetQzoneToMysql(object):
                 info = self.get_time(driver.page_source)
 
                 if info == 'exit':
-                    exit(0)
+                    break
                 else:
                     #nextpage
                     try:
@@ -125,7 +126,6 @@ class GetQzoneToMysql(object):
                         driver.find_element_by_id(self.get_next_page(driver.page_source)).click()
                         time.sleep(3)
                         continue
-
 
         # driver.close()
         # driver.quit()
@@ -143,7 +143,7 @@ class GetQzoneToMysql(object):
         time_re = r'<a class="c_tx c_tx3 goDetail"[\s\S]+?title="([\s\S]+?)"'
         text_re = r'<a href="http://rc.qzone.qq.com/qzonesoso/\?search=%E6%89%BE%E5%AF%B9%E8%B1%A1&amp;entry=99&amp;businesstype=mood" target="_blank">[\s\S]+?找对象#</a>([\s\S]+?)<(/pre|a)'
 
-        info = ' '
+        info = ''
         for i in range(len(li)):
             if re.findall(text_re,str(li[i])) != []:
                 time_ = re.findall(time_re, str(li[i]))
@@ -153,7 +153,7 @@ class GetQzoneToMysql(object):
                     try:
                         info = self.up_mysql(str(li[i]),time_[0],upload_time)
                         if info == 'ok':
-                            print('succeed')
+                            print('succeed1')
                             time.sleep(1)
                     except BaseException as e:
                         print('failed')
@@ -165,13 +165,13 @@ class GetQzoneToMysql(object):
                         break
                 else:
                     print('time error!')
-                    exit(1)
+                    break
 
         return info
 
     def new_time(self,time_,upload_time):
 
-        timeArray = time.strptime(upload_time[0], "%Y年%m月%d日 %H:%M")
+        timeArray = time.strptime(upload_time, "%Y年%m月%d日 %H:%M")
         upload_time_ = time.mktime(timeArray)
         new_time = max(int(time_),int(upload_time_))
 
